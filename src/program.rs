0c6092343f6b608947cda43b3b4f72b3accd5898
Curtion <curtion@126.com>
2020-10-10 00:12:37 +0800

    完成防火墙部分功能

--- src/program.rs
+++ src/program.rs
@@ -1,32 +1,88 @@
 use std::os::windows::process::CommandExt;
 use std::process::Command;
+use textcode::gb2312;
 fn get_hs_path() -> String {
+  // 获取炉石执行程序路径
   const CREATE_NO_WINDOW: u32 = 0x08000000;
   let mut process = Command::new("wmic");
   process
-    .args(&[
-      "Process",
-      "where",
-      // "name='Hearthstone.exe'",
-      "name='QQ.exe'",
-      "get",
-      "executablepath",
-    ])
+    .arg("Process")
+    .arg("where")
+    .arg("name='Hearthstone.exe'")
+    .arg("get")
+    .arg("executablepath")
     .creation_flags(CREATE_NO_WINDOW);
   let output = process.output().expect("获取路径失败");
-  let path = String::from_utf8_lossy(&output.stdout).to_string();
+  let mut path = String::new();
+  gb2312::decode(&output.stdout, &mut path);
   let path: Vec<&str> = path.split_whitespace().collect();
   path[1].to_string()
 }
+
 fn is_fw_rule() -> bool {
-  true
-}
-pub fn start() {
-  let hs_path = get_hs_path();
-  if is_fw_rule() {
-    // 开始任务
+  // 判断规则是否存在
+  let mut process = Command::new("netsh");
+  process
+    .arg("advfirewall")
+    .arg("firewall")
+    .arg("show")
+    .arg("rule")
+    .arg("name=Curtion_LS");
+  let output = process.output().expect("判断路径失败");
+  let mut res = String::new();
+  gb2312::decode(&output.stdout, &mut res);
+  if res.contains("Curtion_LS") {
+    true
   } else {
-    // 先创建规则再开始任务
+    false
   }
-  println!("{}", hs_path);
+}
+
+fn creat_firewall_rule(hs_path: &str) {
+  // 创建规则
+  let mut process = Command::new("netsh");
+  process
+    .arg("advfirewall")
+    .arg("firewall")
+    .arg("add")
+    .arg("rule")
+    .arg("name=Curtion_LS")
+    .arg("dir=out")
+    .arg("action=block")
+    .arg(String::new() + "program=" + hs_path)
+    .arg("enable=NO");
+  process.output().expect("创建规则失败");
+}
+
+async fn start_reconnection() {
+  let mut process = Command::new("netsh");
+  process
+    .arg("advfirewall")
+    .arg("firewall")
+    .arg("set")
+    .arg("rule")
+    .arg("name=Curtion_LS")
+    .arg("new")
+    .arg("new enable=YES");
+  process.output().expect("禁用网络失败");
+
+  let mut process = Command::new("netsh");
+  process
+    .arg("advfirewall")
+    .arg("firewall")
+    .arg("set")
+    .arg("rule")
+    .arg("name=Curtion_LS")
+    .arg("new")
+    .arg("new enable=NO");
+  process.output().expect("恢复网络失败");
+}
+
+pub fn start() {
+  if !is_fw_rule() {
+    // 先创建规则再开始任务
+    let hs_path = get_hs_path();
+    creat_firewall_rule(&hs_path);
+  }
+  start_reconnection();
 }

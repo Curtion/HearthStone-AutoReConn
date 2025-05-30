cfaffba049e8126202f4edee83693c682c6a93c0
Curtion <curtion@126.com>
2025-05-30 14:53:20 +0800

    chore: 初始化项目

--- build.rs
+++ build.rs
@@ -1,22 +1,7 @@
-extern crate winres;
-
 fn main() {
-  if cfg!(target_os = "windows") {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("./src/assets/hsarec.ico");
-    res.set_manifest(
-      r#"
-    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
-    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
-        <security>
-            <requestedPrivileges>
-                <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
-            </requestedPrivileges>
-        </security>
-    </trustInfo>
-    </assembly>
-    "#,
-    );
-    res.compile().unwrap();
-  }
+    if cfg!(target_os = "windows") {
+        embed_resource::compile("manifest.rc", embed_resource::NONE)
+            .manifest_required()
+            .unwrap();
+    }
 }

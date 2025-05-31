1fad7584ece35e7ff0ed76d087f73d45d002e80b
Curtion <curtion@126.com>
2025-05-31 23:52:28 +0800

    feat: 替换全局热键库

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,41 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+ #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+ 
+ use anyhow::Result;
+ use std::sync::mpsc;
+ use tray_item::{IconSource, TrayItem};
+ use inputbot::KeybdKey;
+ 
+ fn main() -> Result<()> {
+     // 用于托盘图标退出事件
+     let (tx, rx) = mpsc::channel();
+ 
+     let mut tray = TrayItem::new("Hsarec", IconSource::Resource("icon"))?;
+     tray.add_menu_item("开始拔线", move || {
+         println!("开始拔线 - 菜单项点击");
+     })?;
+     tray.inner_mut().add_separator()?;
+     tray.add_menu_item("关于我", move || {
+         let _ = webbrowser::open("https://blog.3gxk.net/about.html");
+     })?;
+     let tx_clone_for_exit_menu = tx.clone();
+     tray.add_menu_item("退出程序", move || {
+         let _ = tx_clone_for_exit_menu.send(());
+     })?;
+ 
+     KeybdKey::F12Key.bind(|| {
+         if KeybdKey::LControlKey.is_pressed() && KeybdKey::LShiftKey.is_pressed() {
+             println!("全局热键 Ctrl+Shift+F12 被按下 (inputbot)");
+         }
+     });
+     
+     std::thread::spawn(|| {
+         inputbot::handle_input_events();
+     });
 
-use anyhow::Result;
-use global_hotkey::{
-    hotkey::{Code, HotKey, Modifiers}, GlobalHotKeyEvent, GlobalHotKeyManager
-};
-use std::sync::mpsc;
-use tray_item::{IconSource, TrayItem};
-
-fn main() -> Result<()> {
-    let manager = GlobalHotKeyManager::new()?;
-    let hotkey = HotKey::new(Some(Modifiers::SHIFT), Code::KeyD);
-    manager.register(hotkey)?;
-
-    let (tx, rx) = mpsc::channel();
-
-    let mut tray = TrayItem::new("Hsarec", IconSource::Resource("icon"))?;
-    tray.add_menu_item("开始拔线", move || {
-        println!("开始拔线");
-    })?;
-    tray.inner_mut().add_separator()?;
-    tray.add_menu_item("关于我", move || {
-        let _ = webbrowser::open("https://blog.3gxk.net/about.html");
-    })?;
-    tray.add_menu_item("退出程序", move || {
-        let _ = tx.send(());
-    })?;
-    loop {
-        if let Ok(_) = rx.try_recv() {
-            break;
-        }
-
-        if let Ok(event) = GlobalHotKeyEvent::receiver().try_recv() {
-            println!("{:?}", event);
-        }
-
-        std::thread::sleep(std::time::Duration::from_millis(50));
-    }
-    Ok(())
-}
+     match rx.recv() {
+         Ok(_) => println!("收到退出信号，程序即将关闭。"),
+         Err(_) => println!("退出信号通道发生错误，程序即将关闭。"),
+     }
+     
+     Ok(())
+ }
